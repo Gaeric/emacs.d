06912dfd403e0b0422ffc5781766012c7c7229f7
lantian <1062217098@qq.com>
2020-03-07 18:40:51 +0800

    删除init.el中的部分配置，移至对应的文件中

--- init.el
+++ init.el
@@ -42,23 +42,3 @@
               (toggle-frame-fullscreen))))
 
   (setq gc-cons-threshold normal-gc-cons-threshold))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(safe-local-variable-values '((pyvenv-workon . "my-blog"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-document-title ((t (:foreground "gold1" :height 1.2))))
- '(org-level-1 ((t (:foreground "DodgerBlue1" :weight normal :height 1.0 :blod nil))))
- '(org-level-2 ((t (:foreground "SteelBlue1" :weight normal :height 1.0 :bold nil))))
- '(org-level-3 ((t (:foreground "DeepSkyBlue1" :weight normal :height 1.0 :bold nil))))
- '(org-level-4 ((t (:foreground "SkyBlue1"))))
- '(org-level-5 ((t (:foreground "MediumOrchid1"))))
- '(org-level-6 ((t (:foreground "hot pink"))))
- '(org-level-7 ((t (:foreground "magenta"))))
- '(org-level-8 ((t (:foreground "deep pink")))))
