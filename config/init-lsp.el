fefdb92f09da21b792a0bfdb5b6ca132186386fd
lantian <1062217098@qq.com>
2020-03-12 15:27:53 +0800

    try lsp-mode for lsp

---
+++ config/init-lsp.el
@@ -0,0 +1,15 @@
+;; init-lsp.el -*- coding: utf-8; lexical-binding: t; -*-
+;; Config for Emacs lsp client
+;; Author:  <Gaeric>
+;; URL: https://github.com/Gaeric
+;;
+;; This file is not part of GNU Emacs.
+;;
+;; License: GPLv3
+
+;; lsp-mode
+;; eglot
+;; https://github.com/joaotavora/eglot/issues/369
+
+(add-to-list 'load-path "~/.emacs.d/site-lisp/lsp-mode/")
+(require 'lsp-mode)
