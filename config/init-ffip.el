24c94dbc935987ae03d9e5d670f737582e69e21a
lantian <1062217098@qq.com>
2020-03-12 14:04:51 +0800

    加入find-file-in-project，用于通用的，查找项目中的文件

---
+++ config/init-ffip.el
@@ -0,0 +1,33 @@
+;; init-ffip.el --- find file in project -*- coding: utf-8; lexical-binding: t; -*-
+;; Author:  <Gaeric>
+;; URL: https://github.com/Gaeric
+;;
+;; This file is not part of GNU Emacs.
+;;
+;; License: GPLv3
+
+;; Find file/directory and review Diff/Patch/Commit quickly everywhere.
+;; @see https://github.com/technomancy/find-file-in-project
+
+;; ffip diff for evil-mode
+(defun ffip-diff-mode-hook-setup ()
+    (evil-local-set-key 'normal "p" 'diff-hunk-prev)
+    (evil-local-set-key 'normal "n" 'diff-hunk-next)
+    (evil-local-set-key 'normal "P" 'diff-file-prev)
+    (evil-local-set-key 'normal "N" 'diff-file-next)
+    (evil-local-set-key 'normal (kbd "RET") 'ffip-diff-find-file)
+    (evil-local-set-key 'normal "o" 'ffip-diff-find-file))
+(add-hook 'ffip-diff-mode-hook 'ffip-diff-mode-hook-setup)
+
+;; use fd (rust) replace GNU Find
+(setq ffip-use-rust-fd t)
+
+;; Only for project
+(gaeric-comma-leader-def
+  "ff" 'find-file-in-project
+  "fp" 'find-file-in-project-at-point
+  "fs" 'find-file-in-project-by-selected ;类比于swiper-thing-at-point
+  )
+;; C-h i g (ivy) Enter for more key-binding tips.
+
+(require 'find-file-in-project)
