8bdd849e38c0ca68e5ec31de369525ba2e65f616
lantian <1062217098@qq.com>
2020-03-13 10:09:03 +0800

    加入init-python配置：使用pyvenv控制虚拟环境，使用eglot和pyls完成补全

---
+++ config/init-python.el
@@ -0,0 +1,25 @@
+;; init-python.el -*- coding: utf-8; lexical-binding: t; -*-
+;; Config Python Language with pyvenv and lsp
+;; Author:  <Gaeric>
+;; URL: https://github.com/Gaeric
+;;
+;; This file is not part of GNU Emacs.
+;;
+;; License: GPLv3
+
+;; use pyvenv for virtual python env
+
+(local-require 'pyvenv)
+
+;; 正常情况下不显示modeline
+(setq pyvenv-mode-line-indicator "")
+
+(defun gaeric-virtualenv-setup ()
+  "用于激活虚拟环境，pyvnenv-workon由.dir-locals控制"
+  (pyvenv-mode)
+  (eglot-ensure))
+
+(add-hook 'python-mode-hook 'gaeric-virtualenv-setup)
+
+
+(provide 'init-python)
