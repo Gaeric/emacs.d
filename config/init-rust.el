f3f2cb42faa7cfe7e5a0082aca0b14b4204fc8fc
lantian <1062217098@qq.com>
2020-03-16 16:07:26 +0800

    加入rust-mode的配置文件

---
+++ config/init-rust.el
@@ -0,0 +1,20 @@
+;; init-rust.el -*- coding: utf-8; lexical-binding: t; -*-
+;; The Rust Program Language
+;; Author:  <Gaeric>
+;; URL: https://github.com/Gaeric
+;;
+;; This file is not part of GNU Emacs.
+;;
+;; License: GPLv3
+
+
+(autoload 'rust-mode "rust-mode" "\
+Major mode for Rust code.
+
+\\{rust-mode-map}
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
+
+(provide 'init-rust)
