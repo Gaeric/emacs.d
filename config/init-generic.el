8accb6c479e6b8deb6096c9b3472b3e510ed15f3
Gaeric <106221708@qq.com>
2020-05-10 20:45:37 +0800

    support xclip

--- config/init-generic.el
+++ config/init-generic.el
@@ -26,7 +26,7 @@
 (setq use-dialog-box nil)              ;never pop dialog
 (setq ring-bell-function 'ignore)      ;关闭出错时的提示声
 (setq default-major-mode 'text-mode)   ;设置默认地主模式为TEXT模式
-(setq x-select-enable-clipboard t)     ;支持emacs和外部程序的粘贴
+(setq select-enable-clipboard t)     ;支持emacs和外部程序的粘贴
 (setq ad-redefinition-action 'warn)    ;redefine warning 时进行警告
 (setq resize-mini-windows nil)         ;保持minibuffer window固定高度
 (setq recentf-max-saved-items 2048)    ;设置recentf的默认大小
