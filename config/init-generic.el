0d3d66cf360e781fa07980ca1312b9988ca22bd9
Gaeric <1062217098@qq.com>
2020-06-25 11:38:27 +0800

    修正语法错误

--- config/init-generic.el
+++ config/init-generic.el
@@ -54,10 +54,10 @@
 	       load-path)))))
 
 ;; Only Setting Fonts for GUI
-(if (display-graphic-p)
-    ;; Setting English Font
-    (set-face-attribute
-     'default nil :family "Monospace")
+(when (display-graphic-p)
+  ;; Setting English Font
+  (set-face-attribute
+   'default nil :family "Monospace")
   ;; Setting Chinese Font
   (dolist (charset '(kana han symbol cjk-misc bopomofo))
     (set-fontset-font (frame-parameter nil 'font)
