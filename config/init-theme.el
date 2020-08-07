f1e0a1870cc0b1e289ca55f9fac9c374b5cfd220
Gaeric <1062217098@qq.com>
2020-08-07 23:13:15 +0800

    加入purcell关于theme的配置

--- config/init-theme.el
+++ config/init-theme.el
@@ -8,34 +8,52 @@
 
 ;;; Code:
 
-(require 'doom-themes)
-(require 'doom-themes-ext-org)
-(require 'doom-themes-ext-neotree)
-(require 'doom-themes-ext-treemacs)
+(when (maybe-require-package 'doom-themes)
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+
+  ;; Enable flashing mode-line on errors
+  ;; (doom-themes-visual-bell-config)
+
+  ;; Enable custom neotree theme (all-the-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; or for treemacs users
+  (setq doom-themes-treemacs-theme "doom-colors") ; use the colorful treemacs theme
+  (doom-themes-treemacs-config)
+
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
+
+
+(require-package 'color-theme-sanityinc-solarized)
+(require-package 'color-theme-sanityinc-tomorrow)
+
+;; Don't prompt to confirm theme safety. This avoids problems with
+;; first-time startup on Emacs > 26.3.
+(setq custom-safe-themes t)
+
+;; If you don't customize it, this is the theme you get.
+(setq-default custom-enabled-themes '(sanityinc-tomorrow-bright))
+
+;; Ensure that themes will be applied even if they have not been customized
+(defun reapply-themes ()
+  "Forcibly load the themes listed in `custom-enabled-themes'."
+  (dolist (theme custom-enabled-themes)
+    (unless (custom-theme-p theme)
+      (load-theme theme)))
+  (custom-set-variables `(custom-enabled-themes (quote ,custom-enabled-themes))))
+
+(add-hook 'after-init-hook 'reapply-themes)
+
+;; -------------------------------------------------------------------
+;; circadian load the theme record time
+;; -------------------------------------------------------------------
 (local-require 'circadian)
-
-;; Global settings (defaults)
-(setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-      doom-themes-enable-italic t) ; if nil, italics is universally disabled
-
 ;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each theme
 ;; may have their own settings.
 (setq circadian-themes '(("8:00" . doom-one-light)
                          ("19:30" . doom-vibrant)))
 (circadian-setup)
-;; (load-theme 'doom-vibrant t)
-;; (load-theme 'doom-one-light t)          ;theme for light
-
-;; Enable flashing mode-line on errors
-;; (doom-themes-visual-bell-config)
-
-;; Enable custom neotree theme (all-the-icons must be installed!)
-(doom-themes-neotree-config)
-;; or for treemacs users
-(setq doom-themes-treemacs-theme "doom-colors") ; use the colorful treemacs theme
-(doom-themes-treemacs-config)
-
-;; Corrects (and improves) org-mode's native fontification.
-(doom-themes-org-config)
 
 (provide 'init-theme)
