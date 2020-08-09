325e016e76e92e4cb988e896f9ae4f1f0b31d641
Gaeric <1062217098@qq.com>
2020-08-09 23:50:38 +0800

    配置hl-line-mode及主题

--- custom.el
+++ custom.el
@@ -5,19 +5,10 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(sanityinc-tomorrow-bright))
  '(package-selected-packages
-   '(color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized diff-hl git-commit fullframe magit-todos magit git-timemachine gitconfig-mode gitignore-mode git-blamed gnu-elpa-keyring-update seq))
- '(safe-local-variable-values '((pyvenv-workon . gsg))))
+   '(color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized diff-hl git-commit fullframe magit-todos magit git-timemachine gitconfig-mode gitignore-mode git-blamed gnu-elpa-keyring-update seq)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-document-title ((t (:foreground "gold1" :height 1.2))))
- '(org-level-1 ((t (:foreground "DodgerBlue1" :weight normal :height 1.0 :blod nil))))
- '(org-level-2 ((t (:foreground "SteelBlue1" :weight normal :height 1.0 :bold nil))))
- '(org-level-3 ((t (:foreground "DeepSkyBlue1" :weight normal :height 1.0 :bold nil))))
- '(org-level-4 ((t (:foreground "SkyBlue1"))))
- '(org-level-5 ((t (:foreground "MediumOrchid1"))))
- '(org-level-6 ((t (:foreground "hot pink"))))
- '(org-level-7 ((t (:foreground "magenta"))))
- '(org-level-8 ((t (:foreground "deep pink")))))
+ )
