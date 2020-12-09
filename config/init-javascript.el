3e1b2bb4788ab37ac9bdd6311f5c80dc938a650c
Gaeric <1062217098@qq.com>
2020-12-09 23:43:58 +0800

    提供对tsx更好的支持

--- config/init-javascript.el
+++ config/init-javascript.el
@@ -124,7 +124,12 @@
 
 (add-hook 'typescript-mode-hook #'setup-tide-mode)
 
-(add-to-list 'auto-mode-alist (cons "\\.tsx" 'typescript-mode))
+;; @https://github.com/ananthakumaran/tide/
+(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (string-equal "tsx" (file-name-extension buffer-file-name))
+              (setup-tide-mode))))
 
 (provide 'init-javascript)
 ;;; init-javascript.el ends here
