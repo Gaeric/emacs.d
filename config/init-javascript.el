a403da05d5933eb03acde1371380d297c8bede0e
Gaeric <1062217098@qq.com>
2020-12-10 23:26:50 +0800

    use prettier replace tide-format

--- config/init-javascript.el
+++ config/init-javascript.el
@@ -114,13 +114,18 @@
   ;; company is an optional dependency. You have to
   ;; install it separately via package-install
   ;; `M-x package-install [ret] company`
-  (company-mode +1))
+  (company-mode +1)
+  (when (maybe-require-package 'prettier-js)
+    (prettier-js-mode)))
 
 ;; aligns annotation to the right hand side
 (setq company-tooltip-align-annotations t)
 
 ;; formats the buffer before saving
-(add-hook 'before-save-hook 'tide-format-before-save)
+;; (add-hook 'before-save-hook 'tide-format-before-save)
+
+(with-eval-after-load 'prettier-js
+  (diminish 'prettier-js-mode))
 
 (add-hook 'typescript-mode-hook #'setup-tide-mode)
 
