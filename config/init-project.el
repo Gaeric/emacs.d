50130e4bad8b158f6f4ea4b0e57312e5445fa61c
Gaeric <1062217098@qq.com>
2021-03-17 23:47:58 +0800

    定制project-find-regexp

--- config/init-project.el
+++ config/init-project.el
@@ -7,9 +7,21 @@
 ;;
 ;; License: GPLv3
 
+(defun gaeric/project-find-regexp ()
+  (interactive)
+  (cond ((functionp 'counsel-rg)
+         (counsel-rg (gaeric/symbol-at-reg-point)))
+        ((functionp 'consult-ripgrep)
+         (let ((project (when-let (project (project-current))
+                          (car (project-roots project))))
+               (symbol (gaeric/symbol-at-reg-point)))
+           (consult-ripgrep project symbol)))
+        (t
+         (project-find-regexp))))
+
 (when (macrop 'gaeric-comma-leader-def)
   (gaeric-comma-leader-def
     "gf" 'project-find-file
-    "gs" 'project-find-regexp))
+    "gs" 'gaeric/project-find-regexp))
 
 (provide 'init-project)
