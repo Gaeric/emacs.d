c75db8ff340a95b4109d3e393ff7833c1b9517b3
Gaeric <1062217098@qq.com>
2021-06-12 23:38:06 +0800

    use vertico instand selectrum

--- config/init-complete.el
+++ config/init-complete.el
@@ -10,24 +10,23 @@
 (when (maybe-require-package 'amx)
   (setq-default amx-save-file (expand-file-name ".amx-items" user-emacs-directory)))
 
-(when (maybe-require-package 'selectrum)
-  (setq selectrum-fix-vertical-window-height t)
-  (with-eval-after-load 'selectrum
+;; vertico {
+(when (maybe-require-package 'vertico)
+  (add-hook 'after-init-hook 'vertico-mode)
+
+  (require-package 'orderless)
+  (with-eval-after-load 'vertico
     (amx-mode 1)
-    (define-key selectrum-minibuffer-map (kbd "M-j") 'selectrum-next-candidate)
-    (define-key selectrum-minibuffer-map (kbd "M-k") 'selectrum-previous-candidate))
-  (add-hook 'after-init-hook 'selectrum-mode))
+    (define-key vertico-map (kbd "M-j") 'vertico-next)
+    (define-key vertico-map (kbd "M-k") 'vertico-previous))
 
-;; config completion style with category
-;; use orderless to completion styles
-(when (maybe-require-package 'orderless)
-  (setq selectrum-refine-candidates-function #'orderless-filter))
+  (setq completion-styles '(basic partial-completion orderless))
 
-(when (maybe-require-package 'embark)
-  ;; use C-h to show help after embark-act-noexit
-  (with-eval-after-load 'selectrum
-    (define-key selectrum-minibuffer-map (kbd "C-c C-o") 'embark-export)
-    (define-key selectrum-minibuffer-map (kbd "C-c C-c") 'embark-act)))
+  (when (maybe-require-package 'embark)
+    (with-eval-after-load 'vertico
+      (define-key vertico-map (kbd "C-c C-o") 'embark-export)
+      (define-key vertico-map (kbd "C-c C-c") 'embark-act))))
+;; }
 
 (when (maybe-require-package 'consult)
   (recentf-mode 1)
