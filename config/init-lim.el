d7ac3d5c068907bdc91bdd0c1c2e5c04587db11a
Gaeric <1062217098@qq.com>
2021-08-02 23:21:30 +0800

    load config.el of lim

--- config/init-lim.el
+++ config/init-lim.el
@@ -9,20 +9,7 @@
 ;; Load lim-xixi
 ;; (add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp/lim/"))
 
-(autoload 'lim-use-package "lim-xixi" "Lightly input mehtod xixi")
-
-(register-input-method
- "lim-xixi" "euc-cn" 'lim-use-package
- "淅淅" "淅淅顶功输入法" "lim-xixi.txt")
-(setq default-input-method "lim-xixi")
-
-(defun lim-active-xixi ()
-  (setq lim-punctuation-list (lim-read-punctuation lim-current-scheme))
-  (setq lim-translate-function 'lim-punctuation-translate)
-  (lim-evil-find-mode))
-
-;; 载入输入法时即加载标点相关控制函数，保证不受影响
-(setq lim-load-hook 'lim-active-xixi)
+(load-file "~/.emacs.d/site-lisp/lim/config.el")
 
 (when (macrop 'gaeric-comma-leader-def)
   (gaeric-comma-leader-def
