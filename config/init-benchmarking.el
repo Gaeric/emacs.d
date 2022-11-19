1a51fc349a193ab11a3836ef67b215b2f8f9db5d
Gaeric <1062217098@qq.com>
2022-11-20 01:40:33 +0800

    add my profiler-process function

--- config/init-benchmarking.el
+++ config/init-benchmarking.el
@@ -76,5 +76,11 @@ LOAD-DURATION is the time taken in milliseconds to load FEATURE.")
 (defun gaeric/log-time (param)
   (message "%s: %s" param (format-time-string "%s.%3N")))
 
+
+(defun gaeric/profiler-process (func)
+  (insert (format ";;%s start\n" (gaeric/log-time (identity func))))
+  (with-temp-buffer (funcall func))
+  (insert (format ";;%s end\n" (gaeric/log-time (identity func)))))
+
 (provide 'init-benchmarking)
 ;;; init-benchmarking.el ends here
