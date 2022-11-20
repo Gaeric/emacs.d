ca3f18f90002f8e7e5c48a62c0de84475dd4cedd
Gaeric <1062217098@qq.com>
2022-11-20 15:46:16 +0800

    upgrade rs-module function

--- config/init-rs-module.el
+++ config/init-rs-module.el
@@ -7,7 +7,14 @@
 ;;
 ;; License: GPLv3
 
+(load-file "~/.emacs.d/site-lisp/emacs-module-rs/target/release/libemacs_rs_module.so")
+
+;; For example
+;; (rs-module/load ".emacs.d/site-lisp/lim/target/release/liblim.so")
+;; (lim-count-words (buffer-substring-no-properties (point-min) (point-max)))
+
 (defvar gaeric/rs-module-default-path default-directory)
+
 (defvar gaeric/rs-module-file-dir "dylib")
 
 (defun dos-reload-workaround (path)
@@ -15,14 +22,18 @@
   (let* ((file-abs (expand-file-name path))
          (file-date (format-time-string "%Y%m%d%H%M%S"))
          (file-new (concat (file-name-base file-abs) "_" file-date "." (file-name-extension file-abs))))
-    (if (file-exists-p (concat gaeric-static-file-dir file-new))
+    (if (file-exists-p (concat gaeric/rs-module-file-dir file-new))
         (message "Skipping unmodified file %s" file-abs)
       (copy-file file-abs file-new)
       (let ((default-directory gaeric/rs-module-default-path))
         (rs-module/load (file-relative-name file-new))))))
 
 (defun load-rs-module (path)
-  (rs-module/load (file-relative-name (expand-file-name path) gaeric/rs-module-default-path)))
-
+  (if (eq system-type 'window-nt)
+      (dos-reload-workaround path)
+    (rs-module/load
+     (file-relative-name
+      (expand-file-name path)
+      gaeric/rs-module-default-path))))
 
 (provide 'init-rs-module)
