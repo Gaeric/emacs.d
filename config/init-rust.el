da0e864369777e770f639aeb74417a78c669ac51
Gaeric <1062217098@qq.com>
2023-07-09 00:40:45 +0800

    use rust-ts-mode replace rustic

--- config/init-rust.el
+++ config/init-rust.el
@@ -7,10 +7,13 @@
 ;;
 ;; License: GPLv3
 
-(when (maybe-require-package 'rustic)
-  (setq rustic-lsp-client 'eglot)
-  (remove-hook 'rustic-mode-hook 'flycheck-mode))
+;; (when (maybe-require-package 'rustic)
+;;   (setq rustic-lsp-client 'eglot)
+;;   (remove-hook 'rustic-mode-hook 'flycheck-mode))
 
-(add-to-list 'auto-mode-alist '("\\.rs\\'" . rustic-mode))
+(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-ts-mode))
+
+(add-hook 'rust-ts-mode-hook 'lsp-bridge-mode)
+(add-hook 'rust-ts-mode-hook 'yas-minor-mode)
 
 (provide 'init-rust)
