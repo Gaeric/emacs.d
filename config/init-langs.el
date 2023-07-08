0ec84abec73b8637e0a1ff55bd5f9378f1d23d58
Gaeric <1062217098@qq.com>
2023-07-09 00:41:53 +0800

    config for lsp-bridge

--- config/init-langs.el
+++ config/init-langs.el
@@ -10,41 +10,54 @@
 ;; eglot
 ;; https://github.com/joaotavora/eglot/issues/369
 
+;; --------- eglot config start
 ;; disable some feature such as highlight symbol
 ;; @see https://github.com/joaotavora/eglot/issues/334
-(require-package 'eglot)
+;; (require-package 'eglot)
 
-(setq read-process-output-max (* 1024 1024))
+;; (setq read-process-output-max (* 1024 1024))
 
-(when (maybe-require-package 'eglot)
-  (maybe-require-package 'consult-eglot))
+;; (when (maybe-require-package 'eglot)
+;;   (maybe-require-package 'consult-eglot))
 
-(when (macrop 'gaeric-comma-leader-def)
-  (gaeric-comma-leader-def
-    "en"  'prog-next-error
-    "ep"  'prog-prev-error))
+;; (gaeric-comma-leader-def
+;;     "en" 'prog-next-error
+;;     "ep" 'prog-prev-error)
+
+;; --------- eglot config finish
 
 (setq treesit-extra-load-path '("~/.emacs.d/site-lisp/tree-sitter-module/dist/"))
+
+(maybe-require-package 'markdown-mode)
 
 (when (maybe-require-package 'treesit-auto)
   (require 'treesit-auto)
   (global-treesit-auto-mode))
 
+(when (maybe-require-package 'posframe)
+  (require 'lsp-bridge)
+  ;; (global-lsp-bridge-mode)
+  (define-key acm-mode-map (kbd "M-j") 'acm-select-next)
+  (define-key acm-mode-map (kbd "M-k") 'acm-select-prev)
+  (define-key acm-mode-map (kbd "M-n") nil)
+  (define-key acm-mode-map (kbd "M-p") nil)
 
-;; (maybe-require-package 'tree-sitter)
-;; (maybe-require-package 'tree-sitter-langs)
+  (dolist (hook
+           (list
+            'c-ts-mode-hook
+            'python-ts-mode-hook
+            'rust-ts-mode-hook
+            ))
+    (add-hook hook #'lsp-bridge-mode)))
 
-;; (dolist (hook
-;;          '(rust-mode-hook
-;;            css-mode-hook
-;;            web-mode-hook
-;;            js-mode-hook
-;;            c-mode-common-hook
-;;            python-mode-hook))
-;;   (add-hook hook #'tree-sitter-mode))
 
-;; (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
-
-;; (setq tsc-dyn-get-from '(:compilation))
+(when (macrop 'gaeric-comma-leader-def)
+  (gaeric-comma-leader-def
+    :keymaps lsp-bridge-mode-map
+    "en" 'lsp-bridge-diagnostic-jump-next
+    "ep" 'lsp-bridge-diagnostic-jump-prev
+    "gd" 'lsp-bridge-find-def
+    "gr" 'lsp-bridge-find-references
+    "go" 'lsp-bridge-find-def-other-window))
 
 (provide 'init-langs)
