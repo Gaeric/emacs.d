c4c4e3aa4baaf79cfc83cf2fab3d80767cbdc645
Gaeric <1062217098@qq.com>
2023-07-18 17:09:59 +0800

    use consult-xref releace xref-show-definitions-completing-read

--- config/init-langs.el
+++ config/init-langs.el
@@ -37,8 +37,12 @@
 
 (with-eval-after-load 'xref
   (setq xref-search-program 'ripgrep)
-  (setq xref-show-definitions-function 'xref-show-definitions-completing-read)
-  (setq xref-show-xrefs-function 'xref-show-definitions-completing-read))
+  ;; (setq xref-show-definitions-function 'xref-show-definitions-completing-read)
+  ;; (setq xref-show-xrefs-function 'xref-show-definitions-completing-read)
+
+  (setq xref-show-definitions-function 'consult-xref)
+  (setq xref-show-xrefs-function 'consult-xref)
+  )
 
 (when (macrop 'gaeric-comma-leader-def)
   (gaeric-comma-leader-def
