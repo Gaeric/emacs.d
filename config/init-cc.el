b363d3af183800eb13e91a7060a6911f3ccedc5e
Gaeric <1062217098@qq.com>
2023-07-20 11:29:08 +0800

    use set-default for every file

--- config/init-cc.el
+++ config/init-cc.el
@@ -8,8 +8,8 @@
 ;; License: GPLv3
 (defun gaeric-common-cc-mode-setup ()
   "setup shared by all languages (java/groovy/c++ ...)"
-  (setq c-basic-offset 4)
-  (setq c-ts-mode-indent-offset 4)
+  (setq-default c-basic-offset 4)
+  (setq-default c-ts-mode-indent-offset 4)
   ;; give me NO newline automatically after electric expressions are entered
   (setq c-auto-newline nil)
 
