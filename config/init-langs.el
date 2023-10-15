c26c1300c4d78c40bab74981029eae97d7f3efb2
Gaeric <1062217098@qq.com>
2023-10-15 17:59:10 +0800

    add breadcrumb-mode

--- config/init-langs.el
+++ config/init-langs.el
@@ -85,6 +85,56 @@
   ;; (setq xref-show-xrefs-function 'xref-show-definitions-completing-read)
 
   (setq xref-show-definitions-function 'consult-xref)
-  (setq xref-show-xrefs-function 'consult-xref))
+  (setq xref-show-xrefs-function 'consult-xref)
+  )
+
+(when (macrop 'gaeric-comma-leader-def)
+  (gaeric-comma-leader-def
+    "en" 'prog-next-error
+    "ep" 'prog-prev-error
+    "ef"  'eglot-format
+    "gd" 'xref-find-definitions
+    "gr" 'xref-find-references
+    "go" 'xref-find-definitions-other-window))
+
+;; --------- eglot config finish
+
+(require 'breadcrumb)
+
+(add-hook 'after-init-hook
+          (lambda ()
+            (breadcrumb-mode)))
+
+(setq treesit-extra-load-path '("~/.emacs.d/site-lisp/tree-sitter-module/dist/"))
+
+(when (maybe-require-package 'treesit-auto)
+  (require 'treesit-auto)
+  (global-treesit-auto-mode))
+
+;; (maybe-require-package 'markdown-mode)
+
+;; (require 'lsp-bridge)
+;; (define-key acm-mode-map (kbd "M-j") 'acm-select-next)
+;; (define-key acm-mode-map (kbd "M-k") 'acm-select-prev)
+;; (define-key acm-mode-map (kbd "M-n") 'acm-doc-scroll-down)
+;; (define-key acm-mode-map (kbd "M-p") 'acm-doc-scroll-up)
+
+;; (dolist (hook
+;;          (list
+;;           'c-ts-mode-hook
+;;           'python-ts-mode-hook
+;;           'rust-ts-mode-hook
+;;           'rustic-mode
+;;           ))
+;;   (add-hook hook #'lsp-bridge-mode))
+
+;; (when (macrop 'gaeric-comma-leader-def)
+;;   (gaeric-comma-leader-def
+;;     :keymaps lsp-bridge-mode-map
+;;     "en" 'lsp-bridge-diagnostic-jump-next
+;;     "ep" 'lsp-bridge-diagnostic-jump-prev
+;;     "gd" 'lsp-bridge-find-def
+;;     "gr" 'lsp-bridge-find-references
+;;     "go" 'lsp-bridge-find-def-other-window))
 
 (provide 'init-langs)
