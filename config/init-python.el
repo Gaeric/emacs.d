ba8a00cfe12816f07ab1d71aa118a61cc8fa5015
Gaeric <1062217098@qq.com>
2024-01-10 21:12:30 +0800

    add python indent default value

--- config/init-python.el
+++ config/init-python.el
@@ -13,6 +13,7 @@
 (defun gaeric-virtualenv-setup ()
   "用于激活虚拟环境，pyvnenv-workon由.dir-locals控制"
   (setq-default fill-column 80)
+  (setq-default python-indent-offset 4)
   (auto-fill-mode)
   (pyvenv-mode))
 
