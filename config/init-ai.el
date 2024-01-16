2dec0453a43ef244e833fe000c6f34e33e4ef386
Gaeric <1062217098@qq.com>
2024-01-17 00:28:11 +0800

    add gptel config

--- config/init-ai.el
+++ config/init-ai.el
@@ -13,11 +13,28 @@
 
 (setq gpt-api-key-file "~/.emacs.d/gpt/api-key")
 (setq gptel-proxy "http://127.0.0.1:20171")
-(setq gptel-model "gpt-4")
+;; (setq gptel-model "gpt-4-32k")
+
+(add-hook 'gptel-post-response-functions 'gptel-end-of-response)
 
 (if (file-exists-p gpt-api-key-file)
-    (with-temp-buffer "*gpt-api-key*"
-                      (insert-file-contents gpt-api-key-file)
-                      (setq gptel-api-key (buffer-string))))
+    (with-temp-buffer
+      "*gpt-api-key*"
+      (insert-file-contents gpt-api-key-file)
+      (setq gptel-api-key (buffer-string))))
+
+
+(defun gaeric/gptel-send ()
+  (interactive)
+  (move-end-of-line nil)
+  (insert (if use-hard-newlines hard-newline "\n"))
+  (gptel-send))
+
+(when (macrop 'gaeric-comma-leader-def)
+  (gaeric-comma-leader-def
+    :keymaps 'gptel-mode-map
+    "cc" 'gaeric/gptel-send
+    )
+  )
 
 (provide 'init-ai)
