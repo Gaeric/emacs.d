f52d81d83e8445ca56772b1f05b2779fff9f45e7
Gaeric <1062217098@qq.com>
2025-02-26 17:30:29 +0800

    replace emacs mirrors

--- config/init-elpa.el
+++ config/init-elpa.el
@@ -19,7 +19,6 @@
 (setq package-archives '(("gnu" . "https://mirrors.ustc.edu.cn/elpa/gnu/")
                          ("melpa" . "https://mirrors.ustc.edu.cn/elpa/melpa/")
                          ("nongnu" . "https://mirrors.ustc.edu.cn/elpa/nongnu/")))
-
 
 ;; Work-around for https://debbugs.gnu.org/cgi/bugreport.cgi?bug=34341
 (when (and (version< emacs-version "26.3")
