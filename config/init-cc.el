3065b776109932719dd158462c3dac4a11028a2a
Gaeric <1062217098@qq.com>
2025-06-05 14:50:18 +0800

    update lang cc config

--- config/init-cc.el
+++ config/init-cc.el
@@ -7,13 +7,7 @@
 ;;
 ;; License: GPLv3
 
-(require 'citre)
-(require 'citre-config)
-
-(defun setup-citre-config ()
-  ;; ubuntu workaround
-  (setq citre-ctags-program "ctags-universal"
-        citre-auto-enable-citre-mode-modes '(c-ts-mode)))
+(defcustom gaeric/c-lang-ide 'native "native/citre")
 
 (defun gaeric-common-cc-mode-setup ()
   "setup shared by all languages (java/groovy/c++ ...)"
@@ -22,23 +16,36 @@
   (setq c-auto-newline nil)
   (yas-minor-mode)
   ;make DEL take all previous whitespace with it
-  (c-toggle-hungry-state 1)
-  (setup-citre-config)
-  (setq lsp-bridge-find-def-fallback-function #'citre-jump)
-  (setq lsp-bridge-find-ref-fallback-function #'citre-jump-to-reference))
+  (c-toggle-hungry-state 1))
 
 (defun gaeric/c-ts-mode-setup()
-  (setq-default c-ts-mode-indent-offset 4)
-  (setup-citre-config))
-
-(require-package 'clang-format)
-
-(when (and (eq emacs-lsp-package 'native) (macrop 'gaeric-comma-leader-def))
-  (gaeric-comma-leader-def
-    :keymaps 'citre-mode-map
-    "gr" 'citre-jump-to-reference))
+  (setq-default c-ts-mode-indent-offset 4))
 
 (add-hook 'c-mode-common-hook 'gaeric-common-cc-mode-setup)
 (add-hook 'c-ts-mode-hook 'gaeric/c-ts-mode-setup)
+
+
+(defun gaeric/citre-config()
+  (setq citre-ctags-program "ctags-universal"
+        citre-auto-enable-citre-mode-modes '(c-ts-mode))
+
+  (if (eq emacs-lsp-package 'lsp-bridge)
+      (progn
+        (setq lsp-bridge-find-def-fallback-function #'citre-jump)
+        (setq lsp-bridge-find-ref-fallback-function #'citre-jump-to-reference)))
+
+
+  (when (and (eq emacs-lsp-package 'native) (macrop 'gaeric-comma-leader-def))
+    (gaeric-comma-leader-def
+      :keymaps 'citre-mode-map
+      "gr" 'citre-jump-to-reference)))
+
+
+(unless (eq gaeric/c-lang-ide 'native)
+  (maybe-require-package 'citre)
+  (require-package 'clang-format)
+
+  (add-hook 'c-mode-common-hook 'gaeric/citre-config)
+  (add-hook 'c-ts-mode-hook 'gaeric/citre-config))
 
 (provide 'init-cc)
